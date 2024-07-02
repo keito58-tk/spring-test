8d345e28afe9de6d48ad3e72a4d5353ab6527ad9
keito58-tk <keito1904@outlook.jp>
2024-07-03 04:18:13 +0900

    commit

--- src/main/java/com/example/samuraitravel/service/StripeService.java
+++ src/main/java/com/example/samuraitravel/service/StripeService.java
@@ -1,13 +1,19 @@
 package com.example.samuraitravel.service;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.samuraitravel.form.ReservationRegisterForm;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.StripeObject;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import com.stripe.param.checkout.SessionRetrieveParams;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,6 +21,13 @@ import jakarta.servlet.http.HttpServletRequest;
 public class StripeService {
 	@Value("${stripe.api-key}")
 	private String stripeApiKey;
+	
+	private final ReservationService reservationService;
+	
+	public StripeService(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+	
 	// セッションを作成し、Stripeに必要な情報を返す
 	public String createStripeSession(String houseName, ReservationRegisterForm reservationRegisterForm, HttpServletRequest httpServletRequest) {
 		Stripe.apiKey = stripeApiKey;
@@ -56,4 +69,30 @@ public class StripeService {
 			return "";
 		}
 	}
-}
+	
+	// セッションから予約情報を取得し、ReservationServiceクラスを介してデータベースに登録する
+	public void processSessionCompleted(Event event) {
+		Optional<StripeObject> optionalStripeObject = event.getDataObjectDeserializer().getObject();
+		optionalStripeObject.ifPresentOrElse(stripeObject -> {
+			Session session = (Session)stripeObject;
+			SessionRetrieveParams params = SessionRetrieveParams.builder().addExpand("payment_intent").build();
+			
+			try {
+				session = Session.retrieve(session.getId(), params, null);
+				Map<String, String> paymentIntentObject = session.getPaymentIntentObject().getMetadata();
+				reservationService.create(paymentIntentObject);
+			} catch (StripeException e) {
+				e.printStackTrace();
+			}
+			System.out.println("予約一覧ページの登録処理が成功しました。");
+			System.out.println("Stripe API Version: " + event.getApiVersion());
+			System.out.println("stripe-java Version: " + Stripe.VERSION);
+		}, 
+		() -> {
+			System.out.println("予約一覧ページの登録処理が失敗しました");
+			System.out.println("Stripe API Version: " + event.getApiVersion());
+			System.out.println("stripe-java Version: " + Stripe.VERSION);
+		
+		});
+		}
+	}
